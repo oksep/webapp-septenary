44b5205879bb07d897cec33f708eb1e37a3198ea
RyfThink <ryfthink@gmail.com>
2017-02-08 22:44:38 +0800

    修改启动图消失延时

--- src/assets/pre-bootstrap.js
+++ src/assets/pre-bootstrap.js
@@ -2,6 +2,8 @@
  * Created by renyufeng on 2017/2/8.
  */
 
+const startingTime = new Date().getTime();
+
 // angular框架启动回调
 function onBootstrap() {
     // return;
@@ -11,6 +13,14 @@ function onBootstrap() {
     preBootstrap.className = "loaded";
     // Remove the bootstrap container after the transition has
     // completed (based on the known transition time).
+
+    const currentTime = new Date().getTime();
+
+    var delay = 0;
+    if (currentTime - startingTime < 3 * 1000) {
+        delay = startingTime + 3 * 1000 - currentTime;
+    }
+
     setTimeout(
         function removeLoadingScreen() {
             preBootstrapContainer
@@ -18,7 +28,7 @@ function onBootstrap() {
                 .removeChild(preBootstrapContainer)
             ;
         },
-        300
+        delay
     );
 }
 
