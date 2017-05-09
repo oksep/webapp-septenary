206f9c671bb53584e301e59b75cc594b40da06d0
RyfThink <ryfthink@gmail.com>
2017-05-09 20:08:24 +0800

    update payload

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -10,6 +10,19 @@ import Result from "./result";
 
 const authRouter: Router = Router();
 
+class Payload {
+    id: number; // 用户ID
+    name: string; // 用户名称
+    role: string; // 用户角色
+    iat: number; // jwt的签发时间
+    exp: number; // jwt的过期时间，这个过期时间必须要大于签发时间
+    iss: string; // jwt签发者
+    sub: string; // jwt所面向的用户
+    aud: string; // 接收jwt的一方
+    nbf: string; // 定义在什么时间之前，该jwt都是不可用的.
+    jti: string; // jwt的唯一身份标识，主要用来作为一次性token,从而回避重放攻击。
+}
+
 authRouter.post('/login', (request: Request, response: Response) => {
     console.log(JSON.stringify(request.body));
     let email = request.body.email;
@@ -17,19 +30,13 @@ authRouter.post('/login', (request: Request, response: Response) => {
     let user = User.find(email, password);
 
     if (user) {
-        const payload = {
-            id: user.id,
-            role: user.role,
-            iat: moment().unix(),
-            exp: moment().add(30, 'minute').unix()
-            // iss: jwt签发者
-            // sub: jwt所面向的用户
-            // aud: 接收jwt的一方
-            // exp: jwt的过期时间，这个过期时间必须要大于签发时间
-            // nbf: 定义在什么时间之前，该jwt都是不可用的.
-            // iat: jwt的签发时间
-            // jti: jwt的唯一身份标识，主要用来作为一次性token,从而回避重放攻击。
-        };
+        const payload = new Payload();
+        payload.id = user.id;
+        payload.name = user.name;
+        payload.role = user.role;
+        payload.iat = moment().unix();
+        payload.exp = moment().add(30, 'minute').unix();
+
         response.json(Result.success({
             token: JWT.encode(payload, Config.JWTSecret) // jwt 签发
         }));
